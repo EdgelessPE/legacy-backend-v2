e20e9fa9b0845a6f8d231dd0bff860effef2c04e
Cno <dsyourshy@qq.com>
2024-03-24 23:24:01 +0800

    chore: copy middleware

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const ALIST_ROOT = "https://zfile.edgeless.top";
 export const BASE_PATH = "插件包";
 export const CACHE_INTERVAL = 60 * (60 * 1000);
+
+const API_PREFIX = "/api";
+export const REDIRECT_ROUTE_PATH = `${API_PREFIX}/redirect`;
+export const REDIRECT_URL_TEMPLATE = `${API_PREFIX}/redirect?path={baseUrl}/{scope}/{software}/{fileName}`;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
 import Koa from "koa";
 import Router from "koa-router";
+import { REDIRECT_ROUTE_PATH } from "./constants";
+import { serviceRedirect } from "./services/redirect";
+import { Result } from "ts-results";
 
 const PORT = 3000;
 
 const app = new Koa();
 const router = new Router();
+
+router.get(REDIRECT_ROUTE_PATH, serviceRedirect);
+
+// Result 类型中间件
+app.use(async (ctx, next) => {
+  const result: Result<unknown, string> | undefined = await next();
+  if (result) {
+    if (result.ok) {
+      // 处理 Ok
+      const res = result.val;
+      // 约定如果返回 OkImpl<string> 则进行重定向
+      if (typeof res === "string") {
+        ctx.redirect(res);
+      } else {
+        ctx.body = res;
+      }
+    } else {
+      // 处理 Err
+      ctx.response.status = 500;
+      ctx.body = {
+        msg: result.val,
+      };
+    }
+  } else {
+    ctx.response.status = 404;
+    ctx.body = {
+      msg: "Error:Service matching this path not found",
+    };
+  }
+});
+app.use(router.routes());
+
+console.log(`Listening on port ${PORT}`);
+app.listen(PORT);
